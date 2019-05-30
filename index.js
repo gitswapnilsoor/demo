a3c7e1bc2c7ef050104f657b049066509da024d8
Suresh C <swapnilsoor@gmail.com>
2019-05-30 06:31:21 +0000

    Updated the files to have running container

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
-client.set('visits', 0);
 
 app.get('/', (req, res) => {
-  client.get('visits', (err, visits) => {
-    res.send('Number of visits is ' + visits);
-    client.set('visits', parseInt(visits) + 1);
-  });
+  res.send('How are you doing');
 });
 
 app.listen(8050, () => {
-  console.log('Listening on port 805050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050');
+  console.log('Listening on port 8050');
 });
 
