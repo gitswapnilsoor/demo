afeb651d753f564fcc309ba59ba12c39352146ca
Harshal Pandit <Harshal.Pandit@xoriant.com>
2019-05-31 06:15:19 +0000

    Swapnil

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Good Afternoon, How are you doing Katappa');
+  res.send('Good Afternoon, How are you doing Swapnil?');
 });
 
 app.listen(8050, () => {
